3210f60ca05091b00deaf787dba773738b6afddc
luizgbraga <luizb.07@outlook.com>
2025-05-08 09:45:27 -0300

    Initial Commit

---
+++ AutomaticDispenser/AutomaticDispenserApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutomaticDispenserApp.swift
+//  AutomaticDispenser
+//
+//  Created by Luiz Guilherme Amadi Braga on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AutomaticDispenserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutomaticDispenserTests/AutomaticDispenserTests.swift
@@ -0,0 +1,16 @@
+//
+//  AutomaticDispenserTests.swift
+//  AutomaticDispenserTests
+//
+//  Created by Luiz Guilherme Amadi Braga on 08/05/25.
+//
+
+import Testing
+
+struct AutomaticDispenserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
